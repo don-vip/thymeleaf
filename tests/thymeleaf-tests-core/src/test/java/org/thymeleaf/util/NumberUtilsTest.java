b7597a92787e48ce48c53896ce58d702b47cfd1a
Vincent Privat <vincent.privat@gmail.com>
2026-02-23 01:52:30 +0100

    Integer(int) has been deprecated and marked for removal

--- tests/thymeleaf-tests-core/src/test/java/org/thymeleaf/util/NumberUtilsTest.java
+++ tests/thymeleaf-tests-core/src/test/java/org/thymeleaf/util/NumberUtilsTest.java
@@ -10,28 +10,28 @@ public class NumberUtilsTest {
     @Test
     public void testSequence() {
 
-        Assertions.assertArrayEquals(new Integer[] {new Integer(1),new Integer(2),new Integer(3)}, NumberUtils.sequence(new Integer(1),new Integer(3)));
-        Assertions.assertArrayEquals(new Integer[] {new Integer(1),new Integer(2),new Integer(3)}, NumberUtils.sequence(new Integer(1),new Integer(3), new Integer(1)));
-        Assertions.assertArrayEquals(new Integer[] {new Integer(1),new Integer(3)}, NumberUtils.sequence(new Integer(1),new Integer(3), new Integer(2)));
-        Assertions.assertArrayEquals(new Integer[] {new Integer(3)}, NumberUtils.sequence(new Integer(3),new Integer(3), new Integer(1)));
-        Assertions.assertArrayEquals(new Integer[] {new Integer(3)}, NumberUtils.sequence(new Integer(3),new Integer(3), new Integer(2)));
-        Assertions.assertArrayEquals(new Integer[] {new Integer(3)}, NumberUtils.sequence(new Integer(3),new Integer(3)));
+        Assertions.assertArrayEquals(new Integer[] {Integer.valueOf(1),Integer.valueOf(2),Integer.valueOf(3)}, NumberUtils.sequence(Integer.valueOf(1),Integer.valueOf(3)));
+        Assertions.assertArrayEquals(new Integer[] {Integer.valueOf(1),Integer.valueOf(2),Integer.valueOf(3)}, NumberUtils.sequence(Integer.valueOf(1),Integer.valueOf(3), Integer.valueOf(1)));
+        Assertions.assertArrayEquals(new Integer[] {Integer.valueOf(1),Integer.valueOf(3)}, NumberUtils.sequence(Integer.valueOf(1),Integer.valueOf(3), Integer.valueOf(2)));
+        Assertions.assertArrayEquals(new Integer[] {Integer.valueOf(3)}, NumberUtils.sequence(Integer.valueOf(3),Integer.valueOf(3), Integer.valueOf(1)));
+        Assertions.assertArrayEquals(new Integer[] {Integer.valueOf(3)}, NumberUtils.sequence(Integer.valueOf(3),Integer.valueOf(3), Integer.valueOf(2)));
+        Assertions.assertArrayEquals(new Integer[] {Integer.valueOf(3)}, NumberUtils.sequence(Integer.valueOf(3),Integer.valueOf(3)));
 
-        Assertions.assertArrayEquals(new Integer[] {new Integer(-1),new Integer(-2),new Integer(-3)}, NumberUtils.sequence(new Integer(-1),new Integer(-3)));
-        Assertions.assertArrayEquals(new Integer[] {new Integer(-1),new Integer(-2),new Integer(-3)}, NumberUtils.sequence(new Integer(-1),new Integer(-3), new Integer(-1)));
-        Assertions.assertArrayEquals(new Integer[] {new Integer(-1),new Integer(-3)}, NumberUtils.sequence(new Integer(-1),new Integer(-3), new Integer(-2)));
-        Assertions.assertArrayEquals(new Integer[] {new Integer(-3)}, NumberUtils.sequence(new Integer(-3),new Integer(-3), new Integer(-1)));
-        Assertions.assertArrayEquals(new Integer[] {new Integer(-3)}, NumberUtils.sequence(new Integer(-3),new Integer(-3), new Integer(-2)));
-        Assertions.assertArrayEquals(new Integer[] {new Integer(-3)}, NumberUtils.sequence(new Integer(-3),new Integer(-3)));
+        Assertions.assertArrayEquals(new Integer[] {Integer.valueOf(-1),Integer.valueOf(-2),Integer.valueOf(-3)}, NumberUtils.sequence(Integer.valueOf(-1),Integer.valueOf(-3)));
+        Assertions.assertArrayEquals(new Integer[] {Integer.valueOf(-1),Integer.valueOf(-2),Integer.valueOf(-3)}, NumberUtils.sequence(Integer.valueOf(-1),Integer.valueOf(-3), Integer.valueOf(-1)));
+        Assertions.assertArrayEquals(new Integer[] {Integer.valueOf(-1),Integer.valueOf(-3)}, NumberUtils.sequence(Integer.valueOf(-1),Integer.valueOf(-3), Integer.valueOf(-2)));
+        Assertions.assertArrayEquals(new Integer[] {Integer.valueOf(-3)}, NumberUtils.sequence(Integer.valueOf(-3),Integer.valueOf(-3), Integer.valueOf(-1)));
+        Assertions.assertArrayEquals(new Integer[] {Integer.valueOf(-3)}, NumberUtils.sequence(Integer.valueOf(-3),Integer.valueOf(-3), Integer.valueOf(-2)));
+        Assertions.assertArrayEquals(new Integer[] {Integer.valueOf(-3)}, NumberUtils.sequence(Integer.valueOf(-3),Integer.valueOf(-3)));
 
-        Assertions.assertArrayEquals(new Integer[0], NumberUtils.sequence(new Integer(1),new Integer(3), new Integer(-1)));
-        Assertions.assertArrayEquals(new Integer[0], NumberUtils.sequence(new Integer(-1),new Integer(-3), new Integer(1)));
-        Assertions.assertArrayEquals(new Integer[0], NumberUtils.sequence(new Integer(1),new Integer(3), new Integer(-2)));
-        Assertions.assertArrayEquals(new Integer[0], NumberUtils.sequence(new Integer(-1),new Integer(-3), new Integer(2)));
-        Assertions.assertArrayEquals(new Integer[0], NumberUtils.sequence(new Integer(3),new Integer(1), new Integer(1)));
-        Assertions.assertArrayEquals(new Integer[0], NumberUtils.sequence(new Integer(-3),new Integer(-1), new Integer(-1)));
-        Assertions.assertArrayEquals(new Integer[0], NumberUtils.sequence(new Integer(3),new Integer(1), new Integer(2)));
-        Assertions.assertArrayEquals(new Integer[0], NumberUtils.sequence(new Integer(-3),new Integer(-1), new Integer(-2)));
+        Assertions.assertArrayEquals(new Integer[0], NumberUtils.sequence(Integer.valueOf(1),Integer.valueOf(3), Integer.valueOf(-1)));
+        Assertions.assertArrayEquals(new Integer[0], NumberUtils.sequence(Integer.valueOf(-1),Integer.valueOf(-3), Integer.valueOf(1)));
+        Assertions.assertArrayEquals(new Integer[0], NumberUtils.sequence(Integer.valueOf(1),Integer.valueOf(3), Integer.valueOf(-2)));
+        Assertions.assertArrayEquals(new Integer[0], NumberUtils.sequence(Integer.valueOf(-1),Integer.valueOf(-3), Integer.valueOf(2)));
+        Assertions.assertArrayEquals(new Integer[0], NumberUtils.sequence(Integer.valueOf(3),Integer.valueOf(1), Integer.valueOf(1)));
+        Assertions.assertArrayEquals(new Integer[0], NumberUtils.sequence(Integer.valueOf(-3),Integer.valueOf(-1), Integer.valueOf(-1)));
+        Assertions.assertArrayEquals(new Integer[0], NumberUtils.sequence(Integer.valueOf(3),Integer.valueOf(1), Integer.valueOf(2)));
+        Assertions.assertArrayEquals(new Integer[0], NumberUtils.sequence(Integer.valueOf(-3),Integer.valueOf(-1), Integer.valueOf(-2)));
 
     }
 
